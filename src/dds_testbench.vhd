2a9586ce20dd501a3442bb1acdb9545b8099a26c
moritz <62518490+frischerZucker@users.noreply.github.com>
2025-08-21 14:16:52 +0200

    Unbenutze Signal im algorithmischen Modelle entfernen & Repo aufräumen.

--- src/dds_testbench.vhd
+++ src/dds_testbench.vhd
@@ -14,6 +14,7 @@ begin
 -- Instanz des zu testenden Modells erzeugen.
 dut: entity work.dds(algorithmisch) port map(T_clk, T_f_sel, T_pwm_out);
 
+-- Stellt die jeweiligen Use-Cases ein.
 tests : process
 begin
     report "Use-Case #1: maximale Frequenz" severity note;
@@ -47,6 +48,7 @@ golden_model: entity work.dds(algorithmisch) port map(T_clk, T_f_sel, T_pwm_out_
 -- Instanz des zu testenden Modells erzeugen.
 dut: entity work.dds(rt) port map(T_clk, T_f_sel, T_pwm_out);
 
+-- Generierung der Clock mit 50 MHz.
 clk_gen: process
 begin
 	T_clk <= '0';
@@ -55,6 +57,7 @@ begin
 	wait for 10 ns;
 end process clk_gen;
 
+-- Stellt die jeweiligen Use-Cases ein.
 use_cases : process
 begin
     report "Use-Case #1: maximale Frequenz" severity note;
@@ -73,11 +76,12 @@ begin
     assert (true = false) report "Alle Use-Cases abgeschlossen." severity failure;
 end process use_cases;
 
+-- Testet (1x pro Taktzyklus) ob Golden Model und DUT die gleichen Ausgangssignale erzeugen.
 tests: process
 begin
 	if not (T_pwm_out = T_pwm_out_golden_model) then
             assert false report "Modelle stimmen nicht überein!" severity warning;
-            assert_count <= assert_count + 1;
+            assert_count <= assert_count + 1; -- Zählt die Fehler.
     end if;
 	wait for 20 ns;
 end process tests;
